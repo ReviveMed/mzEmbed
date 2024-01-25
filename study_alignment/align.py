2f1a8b40e32635e1b797a9aedb1dd2e33e0ad38e
Jonah Eaton <49370794+jonaheaton@users.noreply.github.com>
2024-01-25 11:16:01 -0500

    changed the main file for peak alignment to be align.py

--- study_alignment/align.py
+++ study_alignment/align.py
@@ -5,8 +5,9 @@ from .utils_eclipse import align_ms_studies_with_Eclipse
 from .utils_metabCombiner import align_ms_studies_with_metabCombiner
 from .mspeaks import MSPeaks
 from .utils_targets import process_targeted_data
+from .utils_misc import load_json, save_json
 
-
+# %% align_ms_studies
 def align_ms_studies(origin_study, input_study, origin_name='origin', input_name='input',
                      alignment_method='metabcombiner', alignment_params=None, save_dir=None):
     '''
@@ -62,11 +63,38 @@ def align_ms_studies(origin_study, input_study, origin_name='origin', input_name
     return alignment_result_df
 
 
+# %% initialize_study_pair
+def initialize_study_pair(peaks_obj_path0,peaks_obj_path1,save_dir,**kwargs):
 
-def initialize_study_pair():
+    mz_tol = kwargs.get('mz_tol', 0.005)
+    yes_plot = kwargs.get('yes_plot', False)
+    name0 = kwargs.get('name0', None)
+    name1 = kwargs.get('name1', None)
+    targets_path0 = kwargs.get('targets_path0', None)
+    targets_path1 = kwargs.get('targets_path1', None)
+
+    input_info_json_file = os.path.join(save_dir, 'input_info.json')
+    if (os.path.exists(input_info_json_file)) and (not yes_plot):
+        print('input info already exists')
+        input_info = load_json(input_info_json_file)
+        # overwrite the comparison id
+        input_info['comparison_id'] = f'{name0}_vs_{name1}'
+        save_json(input_info, input_info_json_file)
+        return input_info
+
+
+    # Choose the names of the studies
+    if name0 is None:
+        name0 = 'origin'
+    if name1 is None:
+        name1 = 'input'
+    if name0 == name1:
+        raise ValueError('name0 and name1 cannot be the same')
+
+
     pass
 
-
+# %% initialize_study_pair_with_targets
 def initialize_study_pair_with_targets():
     pass
 
