699b7e59f1c017d924b257692ddd491069d73c82
Jonah Eaton <jeaton@revivemed.io>
2024-04-02 13:08:10 -0400

    update eval plots for AE

--- ml/top_model_eval.py
+++ ml/top_model_eval.py
@@ -2,17 +2,17 @@ NEPTUNE_API_TOKEN = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGl
 
 import neptune
 import os
-import optuna
-import json
-from prep_study import add_runs_to_study, reuse_run, convert_neptune_kwargs, \
-    objective_func1, make_kwargs, convert_distributions_to_suggestion, convert_model_kwargs_list_to_dict,\
-    round_kwargs_to_sig
+# import optuna
+# import json
+# from prep_study import add_runs_to_study, reuse_run, convert_neptune_kwargs, \
+#     objective_func1, make_kwargs, convert_distributions_to_suggestion, convert_model_kwargs_list_to_dict,\
+#     round_kwargs_to_sig
 from setup2 import setup_neptune_run
 from misc import download_data_dir
-from utils_neptune import  get_run_id_list, check_neptune_existance
-from sklearn.linear_model import LogisticRegression
-import time
-from neptune.exceptions import NeptuneException
+# from utils_neptune import  get_run_id_list, check_neptune_existance
+# from sklearn.linear_model import LogisticRegression
+# import time
+# from neptune.exceptions import NeptuneException
 
 
 
@@ -42,23 +42,23 @@ kwargs = {}
 ###############################
 ### Plot the latent space
 kwargs['overwrite_existing_kwargs'] = True
-# kwargs['encoder_kind'] = 'AE'
+kwargs['encoder_kind'] = 'AE'
 kwargs['load_model_loc'] = 'pretrain'
 kwargs['run_evaluation'] = False
 kwargs['run_training'] = False
-# kwargs['save_latent_space'] = True
-kwargs['save_latent_space'] = False
+kwargs['save_latent_space'] = True
+# kwargs['save_latent_space'] = False
 
 kwargs['plot_latent_space'] = 'plotly' #'sns' #'both'
 kwargs['plot_latent_space_cols'] = ['Study ID','Cohort Label','is Pediatric']
 
 # run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
 
-# kwargs['eval_name'] = 'train'
-# run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
+kwargs['eval_name'] = 'train'
+run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
 
-# kwargs['eval_name'] = 'test'
-# run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
+kwargs['eval_name'] = 'test'
+run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
 
 kwargs['eval_name'] = 'val'
 run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
