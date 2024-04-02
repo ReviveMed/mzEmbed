8bbb8eda9272c127cc0209fd5ab2624223429bcb
Jonah Eaton <jeaton@revivemed.io>
2024-04-02 12:54:12 -0400

    customize to plot the TGEM encoder

--- ml/top_model_eval.py
+++ ml/top_model_eval.py
@@ -27,26 +27,29 @@ if not os.path.exists(data_dir+'/X_pretrain_train.csv'):
     download_data_dir(data_url, save_dir=data_dir)
 
 # run_id = 'RCC-1296'
-run_id = 'RCC-1216'
+run_id = 'RCC-1216' # this was TGEM Ecnoder!
 kwargs = {}
 
 ###############################
 ### Plot the latent space
 kwargs['overwrite_existing_kwargs'] = True
+# kwargs['encoder_kind'] = 'AE'
 kwargs['load_model_loc'] = 'pretrain'
 kwargs['run_evaluation'] = False
 kwargs['run_training'] = False
-kwargs['save_latent_space'] = True
+# kwargs['save_latent_space'] = True
+kwargs['save_latent_space'] = False
+
 kwargs['plot_latent_space'] = 'sns' #'both'
 kwargs['plot_latent_space_cols'] = ['Study ID','Cohort Label','is Pediatric']
 
 # run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
 
-kwargs['eval_name'] = 'train'
-run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
+# kwargs['eval_name'] = 'train'
+# run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
 
-kwargs['eval_name'] = 'test'
-run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
+# kwargs['eval_name'] = 'test'
+# run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
 
 kwargs['eval_name'] = 'val'
 run_id = setup_neptune_run(data_dir,setup_id='pretrain',with_run_id=run_id,**kwargs)
