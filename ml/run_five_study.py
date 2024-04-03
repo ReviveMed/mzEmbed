a26e2e73aa3f7a69f918f2ef38009515bb5158da
Jonah Eaton <jeaton@revivemed.io>
2024-04-03 21:16:43 +0000

    four part optuna optimization

--- ml/run_five_study.py
+++ ml/run_five_study.py
@@ -22,7 +22,7 @@ USE_WEBAPP_DB = True
 SAVE_TRIALS = True
 WEBAPP_DB_LOC = 'mysql://root:zm6148mz@34.134.200.45/mzlearn_webapp_DB'
 
-ADD_EXISTING_RUNS_TO_STUDY = False
+ADD_EXISTING_RUNS_TO_STUDY = True
 limit_add = 0 # limit the number of runs added to the study
 
 # get user input
