45bc5b3f349e5c228672c40bc04498238a0e12e5
Jonah Eaton <jeaton@revivemed.io>
2024-04-04 10:31:36 -0400

    cleanup the finetune script and choose best runs with 4 objectives

--- ml/run_mkscc_finetune.py
+++ ml/run_mkscc_finetune.py
@@ -154,7 +154,7 @@ def compute_mskcc_finetune(run_id,plot_latent_space=False,
             kwargs['train_kwargs']['head_weight'] = 1
             kwargs['train_kwargs']['encoder_weight'] = 0
             kwargs['train_kwargs']['adversary_weight'] = 0
-            kwargs['train_kwargs']['learning_rate'] = 0.001
+            kwargs['train_kwargs']['learning_rate'] = 0.0001
             kwargs['train_kwargs']['l2_reg_weight'] = 0.0005
             kwargs['train_kwargs']['l1_reg_weight'] = 0.005
             kwargs['train_kwargs']['noise_factor'] = 0.1
@@ -190,8 +190,8 @@ def compute_mskcc_finetune(run_id,plot_latent_space=False,
                         capture_hardware_metrics=False)
         
         metric_string_dct = {}
-        metric_string_dct['MSKCC Train AUROC'] = 'eval/train/Binary_MSKCC/AUROC (micro)'
-        metric_string_dct['MSKCC Val AUROC'] = 'eval/val/Binary_MSKCC/AUROC (micro)'
+        metric_string_dct['Train AUROC'] = 'eval/train/Binary_MSKCC/AUROC (micro)'
+        metric_string_dct['Val AUROC'] = 'eval/val/Binary_MSKCC/AUROC (micro)'
         # metric_calcs = ['mean','std','count']
         metric_vals_dct = defaultdict(list)
 
@@ -227,16 +227,18 @@ if __name__ == '__main__':
     already_run = []
     # run_id_list = ['RCC-1296']
     # run_id_list = ['RCC-924','RCC-973','RCC-938','RCC-931','RCC-984','RCC-933','RCC-1416','RCC-1364','RCC-1129']
-    run_id_list = get_run_id_list(tags=['april03_pareto'],encoder_kind='AE')
-    for run_id in run_id_list[:2]:
+    run_id_list = get_run_id_list(tags=['april04_pareto'],encoder_kind='AE')
+    for run_id in run_id_list:
         if run_id in already_run:
             continue
         print('run_id:',run_id)
         try:
-            run_id = compute_mskcc_finetune(run_id,n_trials=0,plot_latent_space=True)
+            run_id = compute_mskcc_finetune(run_id,n_trials=5,plot_latent_space=True,str_desc='Apr04_MSKCC')
         except NeptuneException as e:
             print('NeptuneException:',e)
             continue
+
+
 
 
     # clean up the previously failed runs #RCC-1132, RCC-1126
