f2ed991fc8f976d0d2cbcc83921f846b43a8ea17
Jonah Eaton <jeaton@revivemed.io>
2024-04-10 22:28:46 +0000

    stuf

--- ml/run_pretrain_study.py
+++ ml/run_pretrain_study.py
@@ -19,7 +19,7 @@ USE_WEBAPP_DB = True
 SAVE_TRIALS = True
 WEBAPP_DB_LOC = 'mysql://root:zm6148mz@34.134.200.45/mzlearn_webapp_DB'
 
-ADD_EXISTING_RUNS_TO_STUDY = True
+ADD_EXISTING_RUNS_TO_STUDY = False
 limit_add = -1 # limit the number of runs added to the study
 
 import sys
