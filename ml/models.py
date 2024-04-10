fixed VAE exploding gradients issue with gradient clipping and weight initialization

ml/models.py
@@ -63,6 +63,15 @@ def _reset_params(layer):
             for child in layer.children():
                 _reset_params(child)
 
+def _init_weights_xavier(layer,gain=1.0):
+    if hasattr(layer, '.weight'):
+        # nn.init.xavier_normal_(layer.weight.data,gain=gain)
+        nn.init.xavier_uniform_(layer.weight.data,gain=gain)
+    else:
+        if hasattr(layer, 'children'):
+            for child in layer.children():
+                _init_weights_xavier(child,gain=gain)
+
 def get_reg_penalty_slow(model,l1_lambda,l2_lambda):
     #this is probably slower, but more memory efficient 
     reg_loss = torch.tensor(0, dtype=torch.float32)
@@ -134,7 +143,7 @@ class Dense_Layers(nn.Module):
             if key not in ['input_size', 'hidden_size', 'output_size', 'num_hidden_layers', 'dropout_rate', 'activation', 'use_batch_norm', 'act_on_output_layer']:
                 print(f'Warning: {key} is not a valid argument for Dense_Layers')
 
-        if activation == 'leakyrelu':
+        if activation == 'leakyrelu' or activation == 'leaky_relu':
             activation_func = nn.LeakyReLU()
         elif activation == 'relu':
             activation_func = nn.ReLU()
@@ -202,7 +211,7 @@ class Dense_Layers(nn.Module):
 class VAE(nn.Module):
     def __init__(self, input_size, hidden_size, latent_size,
                  num_hidden_layers=1, dropout_rate=0,
-                 activation='leakyrelu', use_batch_norm=False, act_on_latent_layer=False):
+                 activation='leaky_relu', use_batch_norm=False, act_on_latent_layer=False):
         super(VAE, self).__init__()
         self.goal = 'encode'
         self.kind = 'VAE'
@@ -211,9 +220,13 @@ class VAE(nn.Module):
         self.hidden_size = hidden_size
         self.num_hidden_layers = num_hidden_layers
         self.dropout_rate = dropout_rate
-        self.activation = activation
+        if activation == 'leakyrelu':
+            self.activation = 'leaky_relu'
+        else:
+            self.activation = activation
         self.use_batch_norm = use_batch_norm
         self.act_on_latent_layer = act_on_latent_layer
+        self.kl_weight = 1.0
 
         self.encoder = Dense_Layers(input_size=input_size, 
                                     hidden_size=hidden_size, 
@@ -240,7 +253,14 @@ class VAE(nn.Module):
         # self.decoder = Dense_Layers(latent_size, hidden_size, input_size,
         #                             num_hidden_layers, dropout_rate, activation,
         #                             use_batch_norm)
-        
+
+    def init_layers(self):
+        # Weight Initialization: Proper weight initialization can help mitigate 
+        # the vanishing/exploding gradients problem. 
+        # Techniques like Xavier/Glorot or He initialization can be used
+        _init_weights_xavier(self.encoder,gain=nn.init.calculate_gain(self.activation))
+        _init_weights_xavier(self.decoder,gain=nn.init.calculate_gain(self.activation))  
+
     def reparameterize(self, mu, log_var):
         std = torch.exp(0.5*log_var)
         eps = torch.randn_like(std)
@@ -257,10 +277,19 @@ class VAE(nn.Module):
 
     def loss(self, x, x_recon, mu, log_var):
         # taking the average over the batch helps with stability
-        recon_loss = F.mse_loss(x_recon, x, reduction='sum')
-        kl_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-        return torch.div(torch.add(recon_loss, kl_loss), x.size(0))
-        # return torch.add(recon_loss, kl_loss)
+        # recon_loss = F.mse_loss(x_recon, x, reduction='sum')
+        # kl_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        # return torch.div(torch.add(recon_loss, kl_loss), x.size(0))
+
+        # recon_loss = F.mse_loss(x_recon, x, reduction='mean')
+        # kl_loss = -0.5 * (1 + log_var - mu.pow(2) - log_var.exp()).sum() / (mu.size(0) * mu.size(1))
+        # return torch.add(recon_loss, self.kl_weight*kl_loss)
+    
+        recon_loss = F.mse_loss(x_recon, x, reduction='mean')
+        # print('recon_loss', recon_loss)
+        kl_loss = -0.5 * torch.mean(1 + log_var - mu.pow(2) - log_var.exp())
+        # print('kl_loss', kl_loss)
+        return torch.add(recon_loss, self.kl_weight*kl_loss)
     
     def forward_to_loss(self, x):
         mu, log_var = self.encoder(x).chunk(2, dim=1)
@@ -300,7 +329,7 @@ class VAE(nn.Module):
 class AE(nn.Module):
     def __init__(self, input_size, hidden_size, latent_size,
                  num_hidden_layers=1, dropout_rate=0,
-                 activation='leakyrelu', use_batch_norm=False, act_on_latent_layer=False):
+                 activation='leaky_relu', use_batch_norm=False, act_on_latent_layer=False):
         super(AE, self).__init__()
         self.goal = 'encode'
         self.kind = 'AE'
@@ -309,7 +338,10 @@ class AE(nn.Module):
         self.hidden_size = hidden_size
         self.num_hidden_layers = num_hidden_layers
         self.dropout_rate = dropout_rate
-        self.activation = activation
+        if activation == 'leakyrelu':
+            self.activation = 'leaky_relu'
+        else:
+            self.activation = activation
         self.use_batch_norm = use_batch_norm
         self.act_on_latent_layer = act_on_latent_layer
         self.encoder = Dense_Layers(input_size=input_size, 
@@ -328,7 +360,14 @@ class AE(nn.Module):
                                     dropout_rate = dropout_rate,
                                     activation = activation,
                                     use_batch_norm = use_batch_norm)
-        
+
+    def init_layers(self):
+        # Weight Initialization: Proper weight initialization can help mitigate 
+        # the vanishing/exploding gradients problem. 
+        # Techniques like Xavier/Glorot or He initialization can be used
+        _init_weights_xavier(self.encoder,gain=nn.init.calculate_gain(self.activation))
+        _init_weights_xavier(self.decoder,gain=nn.init.calculate_gain(self.activation))  
+
     def forward(self, x):
         z = self.encoder(x)
         return self.decoder(z)
@@ -1221,6 +1260,10 @@ class TGEM_Encoder(torch.nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
         self.sublayer = res_connect(input_size, dropout_rate)
     
+
+    def init_layers(self):
+        pass
+
     def reset_params(self):
         _reset_params(self)
 

ml/setup2.py
@@ -242,6 +242,8 @@ def setup_neptune_run(data_dir,setup_id,with_run_id=None,run=None,**kwargs):
                     run[f'{load_encoder_loc}/models/encoder_state_dict'].download(local_path)
                 encoder_state_dict = torch.load(local_path)
                 encoder.load_state_dict(encoder_state_dict)
+            else:
+                encoder.init_layers()
 
             ####################################
             ###### Create the Head Models ######
@@ -379,7 +381,8 @@ def setup_neptune_run(data_dir,setup_id,with_run_id=None,run=None,**kwargs):
             #     )
 
             if run_random_init:
-                encoder.reset_params()
+                # encoder.reset_params()
+                encoder.init_layers()
                 head.reset_params()
                 adv.reset_params()
 

ml/train3.py
@@ -143,8 +143,14 @@ def train_compound_model(dataloaders,encoder,head,adversary, run, **kwargs):
     adversarial_mini_epochs = kwargs.get('adversarial_mini_epochs', 20)
     prefix = kwargs.get('prefix', 'train')
     train_name = kwargs.get(f'train_name', 'train')
-
     
+    clip_grads_with_norm = kwargs.get('clip_grads_with_norm', True)
+    clip_grads_with_value = kwargs.get('clip_grads_with_value', False)
+    clip_value = kwargs.get('clip_value', 1)
+
+    if (not clip_grads_with_norm) and (not clip_grads_with_value):
+        print('Warning: clip_grads_with_norm=False and clip_grads_with_value=False: this may lead to exploding gradients')
+    assert clip_value > 0
 
     if phase_list is None:
         phase_list = list(dataloaders.keys())
@@ -247,6 +253,7 @@ def train_compound_model(dataloaders,encoder,head,adversary, run, **kwargs):
     best_wts = {'encoder': encoder.state_dict(), 'head': head.state_dict(), 'adversary': adversary.state_dict()}
     patience_counter = 0
 
+
     # start the training loop
     for epoch in range(num_epochs):
         if encoder_type == 'TGEM_Encoder':
@@ -347,27 +354,17 @@ def train_compound_model(dataloaders,encoder,head,adversary, run, **kwargs):
                     if torch.isnan(encoder_loss):
                         if run_status:
                             print('Encoder loss is nan!')
-                        encoder_loss = torch.tensor(0.0)
+                    else:
+                        run[f'{prefix}/{phase}/batch/encoder_loss'].append(encoder_loss)
 
                     if torch.isnan(head_loss):
                         if run_status:
                             print('Head loss is nan!')
-                        # print('Head loss is nan!')
-                        head_loss = torch.tensor(0.0)
+                    else:
+                        run[f'{prefix}/{phase}/batch/multi_head_loss'].append(head_loss)
 
-                    run[f'{prefix}/{phase}/batch/encoder_loss'].append(encoder_loss)
-                    run[f'{prefix}/{phase}/batch/multi_head_loss'].append(head_loss)
                     run[f'{prefix}/{phase}/batch/multi_adversary_loss'].append(adversary_loss)
 
-                    # new addition, if join loss is 0, then only nans are present
-                    # we don't want to backpropagate
-                    if (encoder_loss.item() == 0) and (head_loss.item() == 0):
-                        if run_status:
-                            print('skipping backprop')
-                            # print('y head output:', y_head_output)
-                            # print('y_head:', y_head.view(-1))
-                            run_status = False
-                        continue
 
                     if isinstance(y_head_output, dict):
                         if isinstance(epoch_head_outputs, dict):
@@ -395,7 +392,8 @@ def train_compound_model(dataloaders,encoder,head,adversary, run, **kwargs):
                         head_weight*head_loss - \
                         adversary_weight*adversary_loss)
 
-                    run[f'{prefix}/{phase}/batch/joint_loss'].append(joint_loss)
+                    if not torch.isnan(joint_loss):
+                        run[f'{prefix}/{phase}/batch/joint_loss'].append(joint_loss)
                     
                     if phase == f'{train_name}':
                         
@@ -410,7 +408,14 @@ def train_compound_model(dataloaders,encoder,head,adversary, run, **kwargs):
                         # joint_loss += get_reg_penalty(adversary, l1_reg_weight, l2_reg_weight)
 
                         joint_loss_w_penalty.backward(retain_graph=True)
-                        
+                        if clip_grads_with_norm:
+                            torch.nn.utils.clip_grad_norm_(encoder.parameters(), clip_value)
+                            torch.nn.utils.clip_grad_norm_(head.parameters(), clip_value)
+                            # torch.nn.utils.clip_grad_value_(encoder.parameters(), 1)
+                        if clip_grads_with_value:
+                            torch.nn.utils.clip_grad_value_(encoder.parameters(), clip_value)
+                            torch.nn.utils.clip_grad_value_(head.parameters(), clip_value)
+
                         encoder_optimizer.step()
                         if head_weight > 0:
                             head_optimizer.step()
@@ -423,11 +428,14 @@ def train_compound_model(dataloaders,encoder,head,adversary, run, **kwargs):
                             adversary_loss.backward()
                             adversary_optimizer.step()
                     
-
-                    running_losses['encoder'] += encoder_loss.item()
-                    running_losses['head'] += head_loss.item()
-                    running_losses['adversary'] += adversary_loss.item()
-                    running_losses['joint'] += joint_loss.item()
+                    if not torch.isnan(encoder_loss):
+                        running_losses['encoder'] += encoder_loss.item()
+                    if not torch.isnan(head_loss):
+                        running_losses['head'] += head_loss.item()
+                    if not torch.isnan(adversary_loss):
+                        running_losses['adversary'] += adversary_loss.item()
+                    if not torch.isnan(joint_loss):
+                        running_losses['joint'] += joint_loss.item()
 
 
             ########  Train the adversary on the fixed latent space for a few epochs