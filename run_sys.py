ce8151e6b996a1a63ce04354addd001855bb3efa
Jonah Eaton <jeaton@revivemed.io>
2024-05-07 13:05:47 -0400

    updated run sys to run optimize multiple traditional models

--- run_sys.py
+++ run_sys.py
@@ -5,17 +5,25 @@ if len(sys.argv)>1:
 else:
     n_trials = 10
 
-if len(sys.argv)>2:
-    run_id = sys.argv[2]
-else:
-    run_id = 'RCC-3011'
+# if len(sys.argv)>2:
+#     run_id = sys.argv[2]
+# else:
+#     run_id = 'RCC-2925'
 
+y_col = 'IMDC BINARY'
 
-os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'both-OS'")
-os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'NIVO-OS'")
-os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'EVER-OS'")
+# os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'both-OS'")
+# os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'NIVO-OS'")
+# os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'EVER-OS'")
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'IMDC' 1")
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'MSKCC' 1")
 
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'IMDC'")
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'MSKCC'")
+
+
+os.system(f"python3 ml/run_traditional_classifier.py --model_name 'xgboost' --n_trials {n_trials} --y_col '{y_col}'")
+# os.system(f"python3 ml/run_traditional_classifier.py --model_name 'logistic_regression' --n_trials {n_trials} --y_col '{y_col}'")
+# os.system(f"python3 ml/run_traditional_classifier.py --model_name 'random_forest' --n_trials {n_trials} --y_col '{y_col}'")
+# os.system(f"python3 ml/run_traditional_classifier.py --model_name 'svc' --n_trials {n_trials} --y_col '{y_col}'")
+# os.system(f"python3 ml/run_traditional_classifier.py --model_name 'decision_tree' --n_trials {n_trials} --y_col '{y_col}'")
