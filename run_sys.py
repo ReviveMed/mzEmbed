526d29a666380c7f1877b0202d56b0bd76fd8a82
Jonah Eaton <jeaton@revivemed.io>
2024-05-07 19:37:07 +0000

    updated run sys

--- run_sys.py
+++ run_sys.py
@@ -10,7 +10,6 @@ else:
 # else:
 #     run_id = 'RCC-2925'
 
-y_col = 'IMDC BINARY'
 
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'both-OS'")
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'NIVO-OS'")
@@ -21,7 +20,18 @@ y_col = 'IMDC BINARY'
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'IMDC'")
 # os.system(f"python3 ml/run_finetune_study.py {n_trials} '{run_id}' 'MSKCC'")
 
+if n_trials == 0:
+    print('evaluate on the top performing datasets')
 
+y_col = 'IMDC BINARY'
+os.system(f"python3 ml/run_traditional_classifier.py --model_name 'xgboost' --n_trials {n_trials} --y_col '{y_col}'")
+os.system(f"python3 ml/run_traditional_classifier.py --model_name 'logistic_regression' --n_trials {n_trials} --y_col '{y_col}'")
+os.system(f"python3 ml/run_traditional_classifier.py --model_name 'random_forest' --n_trials {n_trials} --y_col '{y_col}'")
+os.system(f"python3 ml/run_traditional_classifier.py --model_name 'svc' --n_trials {n_trials} --y_col '{y_col}'")
+os.system(f"python3 ml/run_traditional_classifier.py --model_name 'decision_tree' --n_trials {n_trials} --y_col '{y_col}'")
+
+
+y_col = 'MSKCC BINARY'
 os.system(f"python3 ml/run_traditional_classifier.py --model_name 'xgboost' --n_trials {n_trials} --y_col '{y_col}'")
 os.system(f"python3 ml/run_traditional_classifier.py --model_name 'logistic_regression' --n_trials {n_trials} --y_col '{y_col}'")
 os.system(f"python3 ml/run_traditional_classifier.py --model_name 'random_forest' --n_trials {n_trials} --y_col '{y_col}'")
