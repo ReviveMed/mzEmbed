8f9813ffc35a6b6b3f8dde0a9106fbabf66ff1ec
Jonah Eaton <jeaton@revivemed.io>
2024-05-09 18:44:49 +0000

    plots for surival reports

--- ml/surival_reports_may09.py
+++ ml/surival_reports_may09.py
@@ -116,7 +116,6 @@ def generate_survival_report(desc_str,head_key,pretrained=True,data_dir=None,loc
             params['num_epochs'] = original_kwargs['train_kwargs']['num_epochs']
             params['weight_decay'] = original_kwargs['train_kwargs']['weight_decay']
 
-        
         model.save_info(model_dir, f'VAE {head_key} info.json')
         model.save_state_to_path(model_dir, f'VAE {head_key} state.pt')
 
@@ -146,7 +145,7 @@ def generate_survival_report(desc_str,head_key,pretrained=True,data_dir=None,loc
 def save_full_report(desc_str):
     report_dict = defaultdict(dict)
 
-
+    os.makedirs(f'{output_dir}/surv_report',exist_ok=True)
     for head_key in ['Cox_NIVO OS', 'Cox_EVER OS', 'Cox_OS']:
         print(head_key)
         report_dict[head_key] = generate_survival_report(desc_str,head_key,pretrained=True,data_dir=data_dir,local_dir=local_dir)
@@ -161,11 +160,11 @@ def save_full_report(desc_str):
         final_df = pd.concat([params_df,score_df],axis=1)
         final_df.index = report_df.index
 
-        final_df.to_csv(f'{output_dir}/{desc_str}_survival_report.csv')
+        final_df.to_csv(f'{output_dir}/surv_report/{desc_str}_survival_report.csv')
 
         return final_df
     except: 
-        json.dump(report_dict,open(f'{output_dir}/{desc_str}_survival_report.json','w'),indent=4)
+        json.dump(report_dict,open(f'{output_dir}/surv_report/{desc_str}_survival_report.json','w'),indent=4)
         return
 
 
@@ -178,4 +177,81 @@ desc_str_list = [
             'Optimized_NIVO-OS ADV EVER-OS_finetune', 'Optimized_NIVO-OS ADV EVER-OS_randinit']
 for desc_str in desc_str_list:
     print(desc_str)
-    save_full_report(desc_str)
+    # save_full_report(desc_str)
+
+
+# %%
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_context('talk')
+
+
+
+def generate_survival_umap(task_id,data_dir=None,local_dir=None,use_full_title=True):
+    if data_dir is None:
+        data_dir = '/app/finetune_data'
+    if local_dir is None:
+        local_dir = os.path.expanduser('~/saved_models')
+
+    os.makedirs(f'{output_dir}/plots')
+
+    run = neptune.init_run(project='revivemed/RCC',
+        api_token= NEPTUNE_API_TOKEN,
+        with_id=run_id,
+        mode="read-only")   
+    run_struc= run.get_structure()
+    substruc = get_sub_struc_from_path(run_struc,f'{task_id}')
+    embeds_dir = f'{local_dir}/{run_id}/{task_id}/embed'
+    os.makedirs(embeds_dir,exist_ok=True)
+    
+    for key in substruc.keys():
+        if 'Z_embed' in key:
+            run[f'{task_id}/{key}'].download(f'{embeds_dir}/{key}.csv')
+    run.stop()
+
+
+    for set_name in ['Train','TrainVal','Val','Test']:
+
+        embed_data_file = f'{embeds_dir}/Z_embed_{set_name.lower()}2.csv'
+        if not os.path.exists(embed_data_file):
+            continue
+
+        df_org = pd.read_csv(embed_data_file,index_col=0)
+
+        for hue_col in ['OS','NIVO-OS','EVER-OS']:
+            for remove_no_imdc in [True,False]:
+                df = df_org[(df_org[hue_col].notnull())]
+
+                fig_name = f'{hue_col} on {set_name}'
+                if remove_no_imdc:
+                    df = df[(df['IMDC'].isin(['FAVORABLE','INTERMEDIATE','POOR']))]
+                    fig_name += ' reqIMDC'
+
+                    sns.scatterplot(data=df,x='UMAP1',y='UMAP2',hue='OS',style='OS_Event',style_order=[1,0],hue_norm=(0,60))
+                    # place the legend outside the plot
+                    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+                    if use_full_title:
+                        plt.title(f'{set_name} (N={len(df)})\n{task_id}')
+                    else:
+                        plt.title(f'{set_name} (N={len(df)})')
+
+                    plt.savefig(f'{output_dir}/plots/{task_id}__{fig_name}.png',bbox_inches='tight')
+                    plt.close()
+
+        return
+
+# %%
+
+desc_str_list = [
+            'Optimized_NIVO-OS_finetune','Optimized_NIVO-OS_randinit',
+            'Optimized_EVER-OS_finetune', 'Optimized_EVER-OS_randinit',
+            'Optimized_both-OS_finetune', 'Optimized_both-OS_randinit',
+            'Optimized_NIVO-OS ADV EVER-OS_finetune', 'Optimized_NIVO-OS ADV EVER-OS_randinit']
+
+
+for desc_str in desc_str_list:
+    print(desc_str)
+    save_full_report(desc_str)                    
