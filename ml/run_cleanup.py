ff680d02cf10e7050f6335a2cb7006f730f6c93a
Jonah Eaton <jeaton@revivemed.io>
2024-05-09 17:52:08 -0400

    sklearn classification

--- ml/run_cleanup.py
+++ ml/run_cleanup.py
@@ -7,12 +7,16 @@ NEPTUNE_API_TOKEN = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGl
 # run_id_list = get_run_id_list(encoder_kind='VAE',tag='april15_leila')
 # run_id_list = get_run_id_list(encoder_kind='VAE',tag='may01_best')
 run_id_list = ['RCC-3011','RCC-2925','RCC-2987']
+run_id_list = ['RCC-2925']
 
 
 rem_fields = []
 # rem_fields = ['both-OS_randinit','both-OS_finetune','both-PFS_randinit','both-PFS_finetune']
 # rem_fields = ['nivo-OS ADV ever-OS_finetune','nivo-OS ADV ever-OS_randinit']
-rem_fields = ['optimized_','OptimizedAlt_','NIVO-OS weight-2 AND EVER-OS hhl epc_60']
+# rem_fields = ['optimized_','OptimizedAlt_','NIVO-OS weight-2 AND EVER-OS hhl epc_60']
+rem_fields = ['NIVO-OS weight-5 AND NIVO-PFS weight-5 AND EVER-PFS AND EVER-OS hhl_finetune','NIVO-OS weight-5 AND NIVO-PFS weight-5 AND EVER-PFS AND EVER-OS hhl_randinit',
+              'NIVO-PFS AND EVER-PFS','NIVO-OS AND MSKCC_finetune','NIVO-OS AND MSKCC_randinit','NIVO-OS AND MSKCC ADV EVER-OS hhl_finetune',
+              'NIVO-OS ADV EVER-OS hhl_randinit','NIVO-OS ADV EVER-OS hhl_finetune']
 
 
 for run_id in run_id_list:
