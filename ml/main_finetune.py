387f9736baaccdfe5b4821059f4534c50f3a8fa1
Jonah Eaton <jeaton@revivemed.io>
2024-05-20 10:56:48 -0500

    bug fix

--- ml/main_finetune.py
+++ ml/main_finetune.py
@@ -462,7 +462,7 @@ if __name__ == '__main__':
     for with_id in [3419, 3098, 3162, 3209, 3217, 3223, 3414, 3419]:
         run = neptune.init_run(project=PROJECT_ID,
                                 api_token=NEPTUNE_API_TOKEN,
-                                with_id=with_id,
+                                with_id=f'SUR-{with_id}',
                                 mode='read-only')
         
         original_sweep_kwargs = run['sweep_kwargs'].fetch()
